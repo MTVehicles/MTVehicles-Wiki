558507e91b006fd55d5d323b46a92d9431a96fb4
Joep Rasters <52205375+GamerJoep@users.noreply.github.com>
2022-04-04 22:15:54 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,11 +13,11 @@ module.exports = {
 		['meta', {name: 'theme-color', content: '#25272a'}]
 	],
 	theme: "yuu",
-	yuu: {
-            defaultDarkTheme: true,
-        },
 	themeConfig: {
 		logo: 'https://www.spigotmc.org/data/resource_icons/80/80910.jpg',
+		yuu: {
+			defaultDarkTheme: true,
+		},
 		nav: [
 			{ text: 'Discord', link: 'https://discord.gg/vehicle' },
 			{ text: 'Site', link: 'https://mtvehicles.eu' }
