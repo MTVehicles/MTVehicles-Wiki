5a004817cbdeb06420289d53436cf896c2e8397a
Joep Rasters <52205375+GamerJoep@users.noreply.github.com>
2022-04-04 20:37:39 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -39,5 +39,11 @@ module.exports = {
 				]
 			}
 		]
-	}
+	},
+  postcss: {
+      plugins: [
+        require('css-prefers-color-scheme/postcss'), 
+        require('autoprefixer')
+      ]
+    }
 }
