0b740ecc2bc042524bf61b609d8ad3017b7dfa4a
Joep Rasters <52205375+GamerJoep@users.noreply.github.com>
2022-04-04 20:24:47 +0200

    Create config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+// .vuepress/config.js
+module.exports = {
+	base: '/',
+	postcss: {
+		plugins: [
+			require('css-prefers-color-scheme/postcss'),
+		]
+	},
+	title: 'MT-Wapens',
+	description: 'De officiële MT-Wapens V3 documentation.',
+	head: [
+		['link', { rel: 'icon', href: '/mtwapens.png' }]
+	],
+	theme: "yuu",
+	themeConfig: {
+		logo: '/mtwapens.png',
+		nav: [
+			{ text: 'Discord', link: 'https://discord.gg/AvRpCUZ' },
+			{ text: 'Admin Panel', link: 'https://dash.mtwapens.nl' }
+		],
+		sidebar: [
+			{
+				title: 'MT-Wapens V3',
+				collapsable: false,
+				children: [
+					'/',
+					'/commands/commands',
+					'/api.md'
+				]
+			},
+			{
+				title: 'Configuration',
+				collapsable: false,
+				children: [
+					'/config/config',
+					'/config/weapons',
+					'/config/ammo',
+					'/config/grenades',
+				]
+			}
+		]
+	}
+}
